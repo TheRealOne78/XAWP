e8809f28c547aa4cc885628cc3ed9064a89fbcc0
TheRealOne78 <53640879+TheRealOne78@users.noreply.github.com>
2022-07-31 13:29:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-dependencies="gcc make libimlib2 libx11 libconfig"
-deb-dependencies="gcc make libimlib2-dev libx11-dev libconfig-dev"
+DEPENDENCIES="gcc make libimlib2 libx11 libconfig"
+DEB_DEPENDENCIES="gcc make libimlib2-dev libx11-dev libconfig-dev"
 
 # Check for root
 if [[ "$EUID" != 0 ]]; then
@@ -15,38 +15,38 @@ fi
 # Debian
 if [ -x "$(command -v apt-get)" ]; then
 	printf "apt-get detected\nInstalling dependencies...\n"
-	apt-get install -y $deb-dependencies
+	apt-get install -y $DEB_DEPENDENCIES
 	
 # Arch
 else if [ -x "$(command -v pacman)" ]; then
 	printf "apt-get detected\nInstalling dependencies...\n"
-	pacman -S $dependencies
+	pacman -S $DEPENDENCIES
 
 # RHEL
 else if [ -x "$(command -v rpm)" ]; then
 	printf "apt-get detected\nInstalling dependencies...\n"
-	rpm -i $dependencies
+	rpm -i $DEPENDENCIES
 
 # Gentoo
 else if [ -x "$(command -v emerge)" ]; then
 	printf "Gentoo distribution detected!\n \
-	Automatic package instalation with portage may lead to package conflicts. Please install $dependencies by hand"
+	Automatic package instalation with portage may lead to package conflicts. Please install $DEPENDENCIES by hand"
 
 ##BSD Family
 # FreeBSD
 else if [ -x "$(command -v pkg)" ]; then
 	printf "pkg detected\nInstalling dependencies...\n"
-	pkg install -y $dependencies
+	pkg install -y $DEPENDENCIES
 
 # OpenBSD
 else if [ -x "$(command -v pkg_add)" ]; then
 	printf "pkg detected\nInstalling dependencies...\n"
-	pkg_add $dependencies
+	pkg_add $DEPENDENCIES
 	
 # NetBSD
 else if [ -x "$(command -v pkgin)" ]; then
 	printf "pkg detected\nInstalling dependencies...\n"
-	pkgin -y install $dependencies
+	pkgin -y install $DEPENDENCIES
 	
 fi
 
