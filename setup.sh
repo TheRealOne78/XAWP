c4031d05b7d1d4eb327d69408a868f40ebac7e9a
TheRealOne78 <bajcsielias78@gmail.com>
2022-07-31 16:11:42 +0300

    RPM custom dependencies

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 DEPENDENCIES="gcc make imlib2 libx11 libconfig"
 DEB_DEPENDENCIES="gcc make libimlib2-dev libx11-dev libconfig-dev"
+RPM_DEPENDENCIES="gcc make imlib2 libX11 libconfig"
 BSD_DEPENDENCIES="gcc gmake imlib2 libx11 libconfig"
 # Check for root
 if [[ "$EUID" != 0 ]]; then
@@ -25,12 +26,12 @@ elif [ -x "$(command -v pacman)" ]; then
 # Fedora
 elif [ -x "$(command -v dnf)" ]; then
 	printf "dnf detected\nInstalling dependencies...\n"
-	dnf install -y $DEPENDENCIES
+	dnf install -y $RPM_DEPENDENCIES
 
 # CentOS
 elif [ -x "$(command -v yum)" ]; then
 	printf "yum detected\nInstalling dependencies...\n"
-	yum install -y $DEPENDENCIES
+	yum install -y $RPM_DEPENDENCIES
 
 # Gentoo
 elif [ -x "$(command -v emerge)" ]; then
